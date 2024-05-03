dab0f7c838446debd3abde8aac8dbb2e2037a3bb
alfahad ansari <157451286+AlFahadAnsari@users.noreply.github.com>
2024-05-03 23:54:35 +0530

    Update Carousel.jsx
    img add

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -10,25 +10,25 @@ const Carousel = () => {
     <div className='max-w-screen-2xl container mx-auto w-100 md:px-20 px-6 mb-5'>
         <div className="carousel rounded-box">
   <div className="carousel-item" >
-    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+    <img src="https://source.unsplash.com/1000x700/?nature,cloud" alt="Burger" />
   </div> 
   <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+    <img src="https://source.unsplash.com/1000x700/?nature,nature" alt="Burger" />
   </div> 
   <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+    <img src="https://source.unsplash.com/1000x700/?nature,water" alt="Burger" />
   </div> 
   <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
+    <img src="https://source.unsplash.com/1000x700/?nature,baloon" alt="Burger" />
   </div> 
   <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+    <img src="https://source.unsplash.com/1000x700/?nature,air" alt="Burger" />
   </div> 
   <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+    <img src="https://source.unsplash.com/1000x700/?nature,sunset" alt="Burger" />
   </div> 
   <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+    <img src="https://source.unsplash.com/1000x700/?nature,alone" alt="Burger" />
   </div>
 </div>
     </div>
