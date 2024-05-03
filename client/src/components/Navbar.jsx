4d580223c2719b12e976619a84af4cbd1c17854b
alfahad ansari <157451286+AlFahadAnsari@users.noreply.github.com>
2024-05-03 23:44:11 +0530

    Update Navbar.jsx
    fixed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,11 +11,19 @@ const Navbar = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                      
+
+                          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Templates</a></li>
+                            <li><a>Marketplace</a></li>
+                            <li><a>Discover</a></li>
+                            <li><a>Pricing</a></li>
+                            <li><a>Learn</a></li>
+                        </ul>
+
                     </div>
 
                     {/* title */}
-                    <a className="btn btn-ghost text-2xl font-bol font-semibold ">Alfahad</a>
+                    <a className="btn btn-ghost text-2xl font-bol font-semibold hidden md:block md:mt-4 ">Alfahad</a>
 
 
                     {/* navbar list */}
